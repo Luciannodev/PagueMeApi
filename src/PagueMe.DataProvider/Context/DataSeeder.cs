57a026c76555f681438fdcb907edb6fc63ba64f4
Luciannodev <joseluciano.dev@gmail.com>
2024-05-07 18:00:35 -0300

    configure dockerfile

--- src/PagueMe.DataProvider/Context/DataSeeder.cs
+++ src/PagueMe.DataProvider/Context/DataSeeder.cs
@@ -1,24 +1,36 @@
-﻿using PagueMe.Domain.Entities;
+﻿using Microsoft.Extensions.Logging;
+using PagueMe.Domain.Entities;
 
 namespace PagueMe.DataProvider.Context
 {
     public class DataSeeder
     {
         private readonly ApplicationDbContext _applicationDbContext;
+        private readonly ILogger _logger;
 
-        public DataSeeder(ApplicationDbContext applicationDbContext)
+        public DataSeeder(ApplicationDbContext applicationDbContext,ILogger logger)
         {
             _applicationDbContext = applicationDbContext;
+            _logger = logger;
         }
 
         public void Seed()
         {
-            if (!_applicationDbContext.PaymentStatus.Any())
+
+            try
             {
-                _applicationDbContext.Add(new PaymentStatus() { IdStatus = 1, NameStatus = "Pendente" });
-                _applicationDbContext.Add(new PaymentStatus() { IdStatus = 2, NameStatus = "Pago" });
-                _applicationDbContext.SaveChanges();
+                
+                if (!_applicationDbContext.PaymentStatus.Any())
+                {
+                    _applicationDbContext.Add(new PaymentStatus() { IdStatus = 1, NameStatus = "Pendente" });
+                    _applicationDbContext.Add(new PaymentStatus() { IdStatus = 2, NameStatus = "Pago" });
+                    _applicationDbContext.SaveChanges();
+                }
+            }catch(Exception ex)
+            {
+                throw new Exception("Erro ao popular a base de dados", ex);
             }
+
         }
     }
 }
---
+++ src/PagueMe.DataProvider/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using PagueMe.Domain.Entities;
+
+namespace PagueMe.DataProvider.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Creditor> Creditor { get; set; }
+        public DbSet<Debtor> Debtor { get; set; }
+        public DbSet<Installment> Installment { get; set; }
+        public DbSet<Loan> Loan { get; set; }
+        public DbSet<PaymentStatus> PaymentStatus { get; set; }
+        int SaveChanges();
+        EntityEntry Update(object entity);
+    }
+}
