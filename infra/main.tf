9ef052eab4f25ee51a2cade8dd0d2685c249f9c9
Luciannodev <joseluciano.dev@gmail.com>
2024-05-07 18:50:18 -0300

    change version

--- infra/main.tf
+++ infra/main.tf
@@ -15,11 +15,10 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
-  engine_version       = "5.7"
+  engine_version       = "8.0.28"
   instance_class       = "db.t4g.micro"
   username             = var.username
   password             = var.password
-  parameter_group_name = "default.mysql5.7"
   publicly_accessible  = true
   skip_final_snapshot  = true
 }
