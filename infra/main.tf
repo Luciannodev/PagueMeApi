45ee4094d7b1ebb4cf34cbc3a3248a4bd98b2380
Luciannodev <joseluciano.dev@gmail.com>
2024-05-07 18:45:49 -0300

    rds change

--- infra/main.tf
+++ infra/main.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "default" {
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
+  instance_class       = "db.t4g.micro"
   username             = var.username
   password             = var.password
   parameter_group_name = "default.mysql5.7"
