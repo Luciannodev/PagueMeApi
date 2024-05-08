8521ed6d7335c98d7f885a41851bff63aa73cb8d
Luciannodev <joseluciano.dev@gmail.com>
2024-05-08 08:41:20 -0300

    teste rds

--- infra/main.tf
+++ infra/main.tf
@@ -11,27 +11,27 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_db_instance" "default" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "8.0.35"
-  instance_class       = "db.t3.micro"
-  identifier           = var.db_name
-  username             = var.username
-  password             = var.password
-  parameter_group_name = "default.mysql8.0"
-  publicly_accessible  = true
-  skip_final_snapshot  = true
-  backup_retention_period = 0 
-}
+# resource "aws_db_instance" "default" {
+#   allocated_storage    = 20
+#   storage_type         = "gp2"
+#   engine               = "mysql"
+#   engine_version       = "8.0.35"
+#   instance_class       = "db.t3.micro"
+#   identifier           = var.db_name
+#   username             = var.username
+#   password             = var.password
+#   parameter_group_name = "default.mysql8.0"
+#   publicly_accessible  = true
+#   skip_final_snapshot  = true
+#   backup_retention_period = 0 
+# }
 
 resource "null_resource" "db_setup" {
   provisioner "local-exec" {
-    command = "mysql -v -h ${aws_db_instance.default.address} -u ${var.username} -p ${var.password} ${var.db_name} < ./SQL/Initial.sql"
+    command = "mysql -v -e -h ${paguemedb.cpm0soy8mgs9.ap-south-1.rds.amazonaws.com} -u ${var.username} -p ${var.password} ${var.db_name} < ./SQL/Initial.sql"
   }
   triggers = {
-    db_instance_address = aws_db_instance.default.address
+    db_instance_address = "paguemedb.cpm0soy8mgs9.ap-south-1.rds.amazonaws.com"
   }
 }
   
